5fcd251ffb696355aee5ecafb7d27e1911f16634
Karla Holman <holmankarla@gmail.com>
2018-07-25 16:46:47 -0700

    Final Changes to Mouse Meet Index

--- wp-content/themes/pnwmousemeet/page-pnw-mouse-meet.php
+++ wp-content/themes/pnwmousemeet/page-pnw-mouse-meet.php
@@ -40,26 +40,31 @@
       </div>
     </div>
   <?php endwhile; else : ?>
-    <h1>TEST</h1>
-  <?php endif; ?>
-
-  <!-- Handle Content -->
-  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-      <?php the_content(); ?>
-
-      <!-- Introduction -->
-      <div class="container page-section">
-        <div class="row">
-          <div class="col-md-6">
-            <h2>What is a Mouse Meet?</h2>
-            <?php the_field('intro_text'); ?>
-          </div>
-          <div class="col-md-6">
-            <?php the_field('intro_image'); ?>
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+      <div class="hero-full"<?php echo(has_post_thumbnail() ? ' style="background-image: url(' . get_the_post_thumbnail_url() . ')"' : ''); ?>>
+        <div class="container">
+          <div class="text-overlay col-md-5">
+            <div class="sparkle"></div>
+            <h1><?php the_title(); ?></h1>
+            <p><?php the_content(); ?></p>
           </div>
         </div>
       </div>
-  <?php endwhile; endif; ?>
+    <?php endwhile; endif; ?>
+  <?php endif; ?>
+
+  <!-- Introduction -->
+  <div class="container page-section">
+    <div class="row">
+      <div class="col-md-6">
+        <h2>What is a Mouse Meet?</h2>
+        <?php the_field('intro_text'); ?>
+      </div>
+      <div class="col-md-6">
+        <?php the_field('intro_image'); ?>
+      </div>
+    </div>
+  </div>
 
   <!-- Handle Info -->
   <?php // Get Mouse Meet Popover
