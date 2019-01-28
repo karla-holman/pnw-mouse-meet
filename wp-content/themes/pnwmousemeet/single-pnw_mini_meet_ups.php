8b4d27050b30368b0e3a9d2c2f158c2d8b4a7c89
Karla Holman <holmankarla@gmail.com>
2019-01-27 19:50:31 -0800

    rearrange mini meet

--- wp-content/themes/pnwmousemeet/single-pnw_mini_meet_ups.php
+++ wp-content/themes/pnwmousemeet/single-pnw_mini_meet_ups.php
@@ -169,52 +169,6 @@
       </div> <!-- End Speakers -->
     <?php endif; ?>
 
-    <!-- Resources -->
-    <?php if( have_rows('resources') ): ?>
-      <a name="resources"></a>
-      <div class="container page-section align-center">
-        <div class="row intro-paragraph">
-          <div class="col-md-12">
-            <h2>Books & More</h2>
-            <p class="intro_text"><?php the_field('resource_intro'); ?>
-          </div>
-        </div>
-        <div class="row">
-          <!-- Resources -->
-            <?php while ( have_rows('resources') ) : ?>
-              <?php $resource = get_post(the_row()); ?>
-              <?php $resource_image = wp_get_attachment_image_src( get_post_thumbnail_id( $resource->ID ), 'single-post-thumbnail' ); ?>
-              <div class="col-md-4">
-                <div class="card resource-card">
-                  <?php if($resource->video_embed): ?>
-                    <div class="main-image">
-                      <?php echo $resource->video_embed ?>
-                    </div>
-                  <?php else: ?>
-                    <div class="main-image" style="background-image: url('<?php echo $resource_image[0]; ?>')"></div>
-                  <?php endif; ?>
-                  <div class="card-body">
-                    <!-- Header -->
-                    <?php if($resource->external_link): ?>
-                      <h5 class="card-title"><a href="<?php echo $resource->external_link ?>"><?php echo get_the_title($resource); ?></a></h5>
-                    <?php elseif($resource->file_upload): ?>
-                      <h5 class="card-title"><a target="_blank" href="<?php echo wp_get_attachment_url( $resource->file_upload ) ?>"><?php echo get_the_title($resource); ?></a></h5>
-                    <?php else: ?>
-                      <h5 class="card-title"><?php echo get_the_title($resource); ?></h5>
-                    <?php endif; ?>
-
-                    <!-- Content -->
-                    <?php if (get_post_field('post_content', $resource->ID)): ?>
-                      <p><?php echo get_post_field('post_content', $resource->ID);?></p>
-                    <?php endif; ?>
-                  </div>
-                </div>
-              </div>
-            <?php endwhile; ?>
-        </div><!-- Row -->
-      </div><!-- Container -->
-    <?php endif; ?>
-
     <!-- Location -->
     <a name="event-location"></a>
     <?php $post_object = get_field('location'); ?>
@@ -258,6 +212,54 @@
     <?php else : ?>
       <p><?php esc_html_e( 'Oops! No Location Found.' ); ?></p>
     <?php endif; ?><!-- END Location -->
+
+    <!-- Resources -->
+    <?php if( have_rows('resources') ): ?>
+      <a name="resources"></a>
+      <div class="color-background primary-light">
+        <div class="container page-section align-center">
+          <div class="row intro-paragraph">
+            <div class="col-md-12">
+              <h2>Books & More</h2>
+              <p class="intro_text"><?php the_field('resource_intro'); ?>
+            </div>
+          </div>
+          <div class="row">
+            <!-- Resources -->
+              <?php while ( have_rows('resources') ) : ?>
+                <?php $resource = get_post(the_row()); ?>
+                <?php $resource_image = wp_get_attachment_image_src( get_post_thumbnail_id( $resource->ID ), 'single-post-thumbnail' ); ?>
+                <div class="col-md-4">
+                  <div class="card resource-card">
+                    <?php if($resource->video_embed): ?>
+                      <div class="main-image">
+                        <?php echo $resource->video_embed ?>
+                      </div>
+                    <?php else: ?>
+                      <div class="main-image" style="background-image: url('<?php echo $resource_image[0]; ?>')"></div>
+                    <?php endif; ?>
+                    <div class="card-body">
+                      <!-- Header -->
+                      <?php if($resource->external_link): ?>
+                        <h5 class="card-title"><a href="<?php echo $resource->external_link ?>"><?php echo get_the_title($resource); ?></a></h5>
+                      <?php elseif($resource->file_upload): ?>
+                        <h5 class="card-title"><a target="_blank" href="<?php echo wp_get_attachment_url( $resource->file_upload ) ?>"><?php echo get_the_title($resource); ?></a></h5>
+                      <?php else: ?>
+                        <h5 class="card-title"><?php echo get_the_title($resource); ?></h5>
+                      <?php endif; ?>
+
+                      <!-- Content -->
+                      <?php if (get_post_field('post_content', $resource->ID)): ?>
+                        <p><?php echo get_post_field('post_content', $resource->ID);?></p>
+                      <?php endif; ?>
+                    </div>
+                  </div>
+                </div>
+              <?php endwhile; ?>
+          </div><!-- Row -->
+        </div><!-- Container -->
+      </div>
+    <?php endif; ?>
 
   <?php endwhile; else : ?>
     <p><?php esc_html_e( 'Oops! No Meet Found.' ); ?></p>
