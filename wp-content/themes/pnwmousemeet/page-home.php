31d68158a124052fb06b5470a22cfc56e3eecf3f
Karla Holman <holmankarla@gmail.com>
2019-02-17 16:59:48 -0800

    updates to home page

--- wp-content/themes/pnwmousemeet/page-home.php
+++ wp-content/themes/pnwmousemeet/page-home.php
@@ -146,7 +146,7 @@
         <!-- Introduction -->
         <div class="row intro-paragraph">
          <div class="col-md-8">
-           <h2>Mouse Meet Events</h2>
+           <h2>PNW Mouse Meet Events</h2>
            <p class="intro">
              PNW Mouse Meet produces a wide variety of events big and small for thousands of Disney Fans including Author Events, Meet Ups, Trips, Cruise and our big Annual Summer Event! Follow the links below for details on all the Events from the past and more Events coming soon…
            </p>
@@ -205,30 +205,64 @@
         </div>
       </div>
 
-     <!-- About Us -->
-     <div class="wheel-background">
-       <img class="wheel" src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/wheel.svg">
-       <div class="container">
-         <div class="row d-flex align-content-center">
-           <div class="col-md-9 offset-md-3 about-card card">
-             <div class="card-body">
-               <div class="row">
-                 <div class="col-md-6 text-area">
-                   <h2>About Us</h2>
-                   <p>
-                     <?php the_field('about_us_text'); ?>
-                   </p>
-                   <a href="<?php the_field('about_us_page'); ?>" class="btn btn-info btn-float">Learn More</a>
-                 </div>
-                 <div class="col-md-6 card-img-bottom" style="background-image: url(<?php echo the_field('about_us_image'); ?>)">
+      <!-- Testimonials -->
+      <?php
+        $args = array(
+          'post_type' => 'testimonials',
+          'meta_key' => 'related_event'
+        );
 
-                 </div>
+        $testimonial_query = new WP_Query( $args );
+      ?>
+
+      <a name="guest-responses"></a>
+      <div class="testimonial-background">
+        <img class="mountain" src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/mountain.svg">
+        <div class="container page-section align-center">
+          <div class="row intro-paragraph">
+            <div class="col-md-12">
+              <h2>Guest Feedback</h2>
+            </div>
+          </div>
+
+            <div id="carouselTestimonial" class="carousel slide" data-ride="carousel">
+              <ol class="carousel-indicators">
+                <?php for( $i = 0; $i < $testimonial_query->post_count; $i++ ): ?>
+                  <li data-target="#carouselTestimonial" data-slide-to="0" class="<?php echo ($i == 0 ? 'active' : '') ?>"></li>
+                <?php endfor; ?>
+              </ol>
+              <div class="carousel-inner row w-75 mx-auto">
+                <?php $first_iteration = true; ?>
+                <?php if( $testimonial_query->have_posts() ) : while( $testimonial_query->have_posts() ) : $testimonial_query->the_post(); ?>
+                  <div class="carousel-item <?php echo ($first_iteration ? 'active' : '') ?>">
+                    <?php $first_iteration = false; ?>
+                    <div class="d-block w-100">
+                      <div class="card">
+                        <div class="card-body">
+                          <?php the_content(); ?>
+                        </div>
+                        <div class="card-footer">
+                          <h6><?php the_field('name'); ?></h6>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                <?php endwhile; else : ?>
+                  <h4>No Quotes Available.</h4>
+                <?php endif; wp_reset_postdata(); ?>
               </div>
-             </div>
-           </div>
-         </div>
-       </div>
-     </div>
+              <a class="carousel-control-prev" href="#carouselTestimonial" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a class="carousel-control-next" href="#carouselTestimonial" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a>
+            </div>
+          </div>
+
+      </div>
 
      <!-- Sponsors -->
      <?php
@@ -275,63 +309,29 @@
         </div>
       </div>
 
-      <!-- Testimonials -->
-      <?php
-        $args = array(
-          'post_type' => 'testimonials',
-          'meta_key' => 'related_event'
-        );
-
-        $testimonial_query = new WP_Query( $args );
-      ?>
-
-      <a name="guest-responses"></a>
-      <div class="testimonial-background">
-        <img class="mountain" src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/mountain.svg">
-        <div class="container page-section align-center">
-          <div class="row intro-paragraph">
-            <div class="col-md-12">
-              <h2>Guest Responses</h2>
-            </div>
-          </div>
-
-            <div id="carouselTestimonial" class="carousel slide" data-ride="carousel">
-              <ol class="carousel-indicators">
-                <?php for( $i = 0; $i < $testimonial_query->post_count; $i++ ): ?>
-                  <li data-target="#carouselTestimonial" data-slide-to="0" class="<?php echo ($i == 0 ? 'active' : '') ?>"></li>
-                <?php endfor; ?>
-              </ol>
-              <div class="carousel-inner row w-75 mx-auto">
-                <?php $first_iteration = true; ?>
-                <?php if( $testimonial_query->have_posts() ) : while( $testimonial_query->have_posts() ) : $testimonial_query->the_post(); ?>
-                  <div class="carousel-item <?php echo ($first_iteration ? 'active' : '') ?>">
-                    <?php $first_iteration = false; ?>
-                    <div class="d-block w-100">
-                      <div class="card">
-                        <div class="card-body">
-                          <?php the_content(); ?>
-                        </div>
-                        <div class="card-footer">
-                          <h6><?php the_field('name'); ?></h6>
-                        </div>
-                      </div>
-                    </div>
+      <!-- About Us -->
+      <div class="wheel-background">
+        <img class="wheel" src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/wheel.svg">
+        <div class="container">
+          <div class="row d-flex align-content-center">
+            <div class="col-md-9 offset-md-3 about-card card">
+              <div class="card-body">
+                <div class="row">
+                  <div class="col-md-6 text-area">
+                    <h2>About Us</h2>
+                    <p>
+                      <?php the_field('about_us_text'); ?>
+                    </p>
+                    <a href="<?php the_field('about_us_page'); ?>" class="btn btn-info btn-float">Learn More</a>
                   </div>
-                <?php endwhile; else : ?>
-                  <h4>No Quotes Available.</h4>
-                <?php endif; wp_reset_postdata(); ?>
+                  <div class="col-md-6 card-img-bottom" style="background-image: url(<?php echo the_field('about_us_image'); ?>)">
+
+                  </div>
+               </div>
               </div>
-              <a class="carousel-control-prev" href="#carouselTestimonial" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-              </a>
-              <a class="carousel-control-next" href="#carouselTestimonial" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-              </a>
             </div>
           </div>
-
+        </div>
       </div>
    <?php endwhile; else : ?>
      <p><?php esc_html_e( 'Sorry, no pages matched your criteria.' ); ?></p>
